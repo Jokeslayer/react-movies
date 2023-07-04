fdd400c0bd1756ec03028b9f4fafd09c07e27785
Jokeslayer <aschwartz07901@gmail.com>
2023-07-04 17:04:13 -0600

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link to="/">Order History</Link>
+      &nbsp; | &nbsp;
+      <Link to="/orders/new">New Order</Link>
+      &nbsp; | &nbsp;
+      <Link to="/orders">Order History</Link>
+      &nbsp; | &nbsp;
+      <Link to="/orders/new">New Order</Link>
+    </nav>
+  );
+}
+
+
+
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import LoginPage from '../LoginPage/LoginPage';
+import MoviesListPage from '../MoviesListPage/MoviesListPage';
+import MovieDetailPage from '../MovieDetailPage/MovieDetailPage';
+import ActorListPage from '../ActorListPage/ActorListPage';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
+  const [user, setUser] = useState({});
+
   return (
     <main className="App">
-      App
+      { user ?
+          <>
+            <NavBar />
+            <Routes>
+              {/* Route components in here */}
+              <Route path="/login" element={<LoginPage/>} />
+              <Route path="/movies/list" element={<MoviesListPage/>} />
+              <Route path="/movies/detail" element={<MovieDetailPage/>} />
+              <Route path="/actor/list" element={<ActorListPage/>} />
+            </Routes>
+          </>
+          :
+          <AuthPage />
+      }
     </main>
   );
 }
